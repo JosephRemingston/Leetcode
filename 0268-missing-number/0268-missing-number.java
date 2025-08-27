495da99a67c89e7966a54b54bb33e9a3ebe61539
Joseph Remingston <ljremi@gmail.com>
2025-08-28 00:20:03 +0530

    Time: 6 ms (11.53%), Space: 45.9 MB (7.23%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int missingNumber(int[] nums) {
+        int i = 0;
+        Arrays.sort(nums);
+        for(i = 0 ; i < nums.length ; i++){
+            if(nums[i] != i){
+                return i;
+            }
+        }
+        return i;
+    }
+}
