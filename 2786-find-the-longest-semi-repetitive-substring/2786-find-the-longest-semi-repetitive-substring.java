a90e3c77e15a7d4295193f38a166572e1d993d6b
Joseph Remingston <ljremi@gmail.com>
2025-08-31 23:24:40 +0530

    Time: 5 ms (100%), Space: 45.8 MB (5.93%) - LeetHub

--- 2786-find-the-longest-semi-repetitive-substring/2786-find-the-longest-semi-repetitive-substring.java
+++ 2786-find-the-longest-semi-repetitive-substring/2786-find-the-longest-semi-repetitive-substring.java
@@ -1,28 +1,22 @@
 class Solution {
     public int longestSemiRepetitiveSubstring(String s) {
         int l = 0;
-        int r = 0;
-        int maxLength = 0;
+        int r = 1;
+        int maxLength = 1;
+        int repeatativePairs = 0;
         while(r < s.length()){
-            String subString = s.substring(l , r + 1);
-            int repeatativePairs = numberOfRepetativePairs(subString);
+            if(s.charAt(r) == s.charAt(r - 1)){
+                repeatativePairs++;
+            }
             while(repeatativePairs > 1){
+                if(s.charAt(l) == s.charAt(l + 1)){
+                    repeatativePairs--;
+                }
                 l++;
-                subString = s.substring(l , r + 1);
-                repeatativePairs = numberOfRepetativePairs(subString);
             }
             maxLength = Math.max(maxLength , r - l + 1);
             r++;
         }
         return maxLength;
-    }
-    int numberOfRepetativePairs(String s){
-        int repeatativePairs = 0;
-        for(int i = 0 ; i < s.length() - 1 ; i++){
-            if(s.charAt(i) == s.charAt(i + 1)){
-                repeatativePairs++;
-            }
-        }
-        return repeatativePairs;
     }
 }
