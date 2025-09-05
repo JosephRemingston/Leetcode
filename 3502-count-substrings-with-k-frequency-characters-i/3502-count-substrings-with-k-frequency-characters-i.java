e99722b28b9cba8129a2be903322827b77cd7c79
Joseph Remingston <ljremi@gmail.com>
2025-09-05 14:24:02 +0530

    Time: 1 ms (100%), Space: 42.2 MB (85.82%) - LeetHub

---
+++ 3502-count-substrings-with-k-frequency-characters-i/3502-count-substrings-with-k-frequency-characters-i.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int numberOfSubstrings(String s, int k) {
+        int n = s.length(), res = (n + 1) * n / 2;
+        int[] count = new int[26];
+        for (int i = 0, j = 0; j < n; j++) {
+            char c = s.charAt(j);
+            count[c - 'a']++;
+            while (count[c - 'a'] >= k) {
+                char leftChar = s.charAt(i);
+                count[leftChar - 'a']--;
+                i++;
+            }
+            res -= j - i + 1;
+        }
+        return res;
+    }
+}
