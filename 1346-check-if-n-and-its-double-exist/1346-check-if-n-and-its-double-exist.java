6891563528bc89726deaf42d4a607dcfefdd839e
Joseph Remingston <ljremi@gmail.com>
2026-01-18 23:12:47 +0530

    Time: 8 ms (6.55%), Space: 45.3 MB (12.32%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        Arrays.sort(arr);
+        for(int i = 0 ; i < arr.length ; i++){
+            for(int j = 0 ; j < arr.length ; j++){
+                if(i != j && arr[i] == 2 * arr[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
