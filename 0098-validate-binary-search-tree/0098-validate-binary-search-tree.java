785149a942e36401b0475f11d821aa9098b375b3
Joseph Remingston <ljremi@gmail.com>
2026-04-01 19:51:28 +0530

    Time: 0 ms (100%), Space: 45.2 MB (57.89%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -0,0 +1,18 @@
+class Solution {
+    Long prev = null;
+
+    public boolean isValidBST(TreeNode root) {
+        return inorder(root);
+    }
+
+    public boolean inorder(TreeNode root){
+        if(root == null) return true;
+
+        if(!inorder(root.left)) return false;
+
+        if(prev != null && root.val <= prev) return false;
+        prev = (long) root.val;
+
+        return inorder(root.right);
+    }
+}
